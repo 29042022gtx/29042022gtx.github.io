75abe2e1f7b0839e67a5c59f2b89156ede8b9fec
29042022gtx <150531389+29042022gtx@users.noreply.github.com>
2024-03-02 09:37:46 +0700

    a

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let loginForm = document.querySelector('.login-form');
-
-document.querySelector('#login').onclick = () => {
-    loginForm.classList.toggle('active');
-}
-
-document.querySelector('#close-login-form').onclick = () => {
-    loginForm.classList.toggle('active');
-}
